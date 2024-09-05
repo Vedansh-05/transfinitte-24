20ca9428c8fea8b547b9379024cfc19a968632c9
Vedansh-05 <110123121@nitt.edu>
2024-09-05 15:52:56 +0530

    feat: Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='flex justify-between items-center h-[72px] shrink-0 self-stretch bg-black border-b-[#EDEDED26] border-b border-solid'>
+      <div className='text-[#A1A1A1] text-sm font-normal leading-[17.5px] tracking-[0.35px] uppercase'>OCT 18-20 2024, GJCH</div>
+      <div>
+        <div className='text-white text-center text-xl font-spacegrotesk font-medium leading-5 tracking-[-0.1px] uppercase '>Transfinitte</div>
+        
+      </div>
+    </div>
+  )
+}
+
+export default Header
