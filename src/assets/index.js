bc6a3e22108bbd7b6b440206964375a77f850cc1
Vedansh-05 <110123121@nitt.edu>
2024-09-06 03:18:17 +0530

    feat: Hero component

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,1 +1,15 @@
-import ''
+import numbersvg from "./svg/24.svg";
+import instagramtext from "./svg/instagram-text.svg"
+import registertext from "./svg/register-text.svg"
+import tfhero from "./svg/tf-hero.svg"
+import bubble from "./svg/bubble.svg"
+import aboutus from "./images/aboutus.jpeg"
+
+export {
+    numbersvg,
+    instagramtext,
+    registertext,
+    tfhero,
+    bubble,
+    aboutus
+    };
